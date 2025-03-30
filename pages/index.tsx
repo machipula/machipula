90b2dfc3839a62e6156f0421a7a361d28fdb4273
machipula <subaru.nakagawa@gmail.com>
2025-03-30 12:48:34 +0900

    Fix: index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div style={{ textAlign: 'center', padding: '50px' }}>
       <h1>ようこそ、マチプラへ！</h1>
-      <p>このページは現在開発中です。</p>
+      <p>このページは現在開発中です。しばらくお待ちください</p>
     </div>
   );
 }
